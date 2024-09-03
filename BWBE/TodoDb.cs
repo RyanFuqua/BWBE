15d352b910479e119ccb0cce78d6b6f351072c1a
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-09-03 14:35:44 -0500

    Create basic API example for team to learn from

---
+++ BWBE/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BWBE;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
